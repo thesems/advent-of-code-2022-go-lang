e854fc67f0fa69355d9f1e4d3647153101a58898
Semir Ramovic <semir.ramovic@outlook.com>
2023-06-11 13:43:58 +0200

    code clean-up

--- day3/day3.go
+++ day3/day3.go
@@ -6,54 +6,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 	"unicode"
 )
 
-// short strings: brutorce o(n^2) 4-5x faster than o(2n)
-// go testing lib
-
-func findCommonCharFromTwoStringsBruteForce(comp1 string, comp2 string) (rune, error) {
-	defer duration(track("bruteforce 2"))
-	for _, char1 := range comp1 {
-		for _, char2 := range comp2 {
-			if char1 == char2 {
-				result := char1
-				if unicode.IsLower(char1) {
-					result = char1 - 96
-				} else {
-					result = char1 - 38
-				}
-				return result, nil
-			}
-		}
-	}
-	return 0, errors.New("no common character")
-}
-
-func findCommonCharFromThreeStringsBruteForce(comp1 string, comp2 string, comp3 string) (rune, error) {
-	defer duration(track("bruteforce 3"))
-	for _, char1 := range comp1 {
-		for _, char2 := range comp2 {
-			for _, char3 := range comp3 {
-				if char1 == char2 && char2 == char3 {
-					result := char1
-					if unicode.IsLower(char1) {
-						result = char1 - 96
-					} else {
-						result = char1 - 38
-					}
-					return result, nil
-				}
-			}
-		}
-	}
-	return 0, errors.New("no common character")
-}
-
 func findCommonCharFromTwoStrings(comp1 string, comp2 string) (rune, error) {
 	helper := make(map[int]bool)
-	defer duration(track("optimized 2"))
 	for _, ch := range comp1 {
 		helper[int(ch)] = true
 	}
@@ -70,26 +27,24 @@ func findCommonCharFromTwoStrings(comp1 string, comp2 string) (rune, error) {
 			return result, nil
 		}
 	}
-	return 0, errors.New("no common character")
+	return 0, errors.New("no common characters found")
 }
 
 func findCommonCharFromThreeStrings(comp1 string, comp2 string, comp3 string) (rune, error) {
-	helper := make(map[int]bool)
-	defer duration(track("optimized 3"))
+	helper1 := make(map[int]bool)
+	helper2 := make(map[int]bool)
+
 	for _, ch := range comp1 {
-		helper[int(ch)] = true
+		helper1[int(ch)] = true
 	}
 	for _, ch := range comp2 {
-		_, ok := helper[int(ch)]
-
-		if !ok {
-			helper[int(ch)] = false
-		}
+		helper2[int(ch)] = true
 	}
 	for _, ch := range comp3 {
-		res, ok := helper[int(ch)]
+		_, is_in_comp1 := helper1[int(ch)]
+		_, is_in_comp2 := helper2[int(ch)]
 
-		if ok && res {
+		if is_in_comp1 && is_in_comp2 {
 			result := ch
 			if unicode.IsLower(ch) {
 				result = ch - 96
@@ -99,15 +54,7 @@ func findCommonCharFromThreeStrings(comp1 string, comp2 string, comp3 string) (r
 			return result, nil
 		}
 	}
-	return 0, errors.New("no common character")
-}
-
-func track(msg string) (string, time.Time) {
-	return msg, time.Now()
-}
-
-func duration(msg string, start time.Time) {
-	log.Printf("%v: %v\n", msg, time.Since(start))
+	return 0, errors.New("no common characters found")
 }
 
 func Day3() {
@@ -131,28 +78,22 @@ func Day3() {
 		comp1 := line[:half_size]
 		comp2 := line[half_size:]
 
-		// findCommonCharFromTwoStringsBruteForce(comp1, comp2)
 		common_char, err := findCommonCharFromTwoStrings(comp1, comp2)
 		if err != nil {
-			log.Fatal("not common characters")
+			log.Fatal("No common characters found!")
 		}
 
 		total_priorities += int(common_char)
 
 		if len(group) == 3 {
-			findCommonCharFromThreeStrings(
-				group[0],
-				group[1],
-				group[2],
-			)
-			common_char, err := findCommonCharFromThreeStringsBruteForce(
+			common_char, err := findCommonCharFromThreeStrings(
 				group[0],
 				group[1],
 				group[2],
 			)
 
 			if err != nil {
-				log.Fatal("No common character among three groups.")
+				log.Fatal("No common characters found!")
 			}
 
 			badges_priorities += int(common_char)
@@ -161,5 +102,5 @@ func Day3() {
 	}
 
 	fmt.Println("Part 1: ", total_priorities)
-	fmt.Println("Part 1: ", badges_priorities)
+	fmt.Println("Part 2: ", badges_priorities)
 }
